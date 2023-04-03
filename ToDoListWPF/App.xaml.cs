ada67a73f247e176a549a9190ef756c50e239ef6
username <email>
2023-04-03 16:34:21 +0800

    防止程序崩溃

--- ToDoListWPF/App.xaml.cs
+++ ToDoListWPF/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Prism.Ioc;
+﻿using BBACAPSPP;
+using Prism.Ioc;
+using System.Threading.Tasks;
+using System;
 using System.Windows;
 using ToDoListWPF.Views;
 using total.ViewModels.Dialogs;
@@ -11,6 +14,11 @@ namespace ToDoListWPF
     /// </summary>
     public partial class App
     {
+        public App()
+        {
+            RegisterEvents();
+        }
+
         protected override Window CreateShell()
         {
             return Container.Resolve<LoginView>();
@@ -19,6 +27,83 @@ namespace ToDoListWPF
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
             containerRegistry.RegisterDialog<OperationMessage, OperationMessageViewModel>("NotificationDialog");
+        }
+
+        private void RegisterEvents()
+        {
+            //Task线程内未捕获异常处理事件
+            TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;//Task异常 
+
+            //UI线程未捕获异常处理事件（UI主线程）
+            this.DispatcherUnhandledException += App_DispatcherUnhandledException;
+
+            //非UI线程未捕获异常处理事件(例如自己创建的一个子线程)
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+        }
+
+        private static void TaskScheduler_UnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            try
+            {
+                var exception = e.Exception as Exception;
+                if (exception != null)
+                {
+                    HandleException(exception);
+                }
+            }
+            catch (Exception ex)
+            {
+                HandleException(ex);
+            }
+            finally
+            {
+                e.SetObserved();
+            }
+        }
+
+        //非UI线程未捕获异常处理事件(例如自己创建的一个子线程)      
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                var exception = e.ExceptionObject as Exception;
+                if (exception != null)
+                {
+                    HandleException(exception);
+                }
+            }
+            catch (Exception ex)
+            {
+                HandleException(ex);
+            }
+            finally
+            {
+                //ignore
+            }
+        }
+
+        //UI线程未捕获异常处理事件（UI主线程）
+        private static void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                HandleException(e.Exception);
+            }
+            catch (Exception ex)
+            {
+                HandleException(ex);
+            }
+            finally
+            {
+                e.Handled = true;
+            }
+        }
+        private static void HandleException(Exception ex)
+        {
+            // MessageBox.Show("出错了，请与开发人员联系："+ ex.Message);
+            //记录日志
+            Utils.LogWrite(ex);
+
         }
     }
 }
